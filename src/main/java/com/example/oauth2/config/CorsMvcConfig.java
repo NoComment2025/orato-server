ea82330c67e8393103c55881e220cf4dbc9a363d
hanbi10 <jeonghanbi10@dgsw.hs.kr>
2025-10-21 20:18:14 +0900

    OAuth2 프로젝트 시작

---
+++ src/main/java/com/example/oauth2/config/CorsMvcConfig.java
@@ -0,0 +1,16 @@
+package com.example.oauth2.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .exposedHeaders("Set-Cookie")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
---
+++ src/main/java/com/example/oauth2/domain/User.java
@@ -0,0 +1,18 @@
+package com.example.oauth2.domain;
+
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User implements Serializable {
+}
