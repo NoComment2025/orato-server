fd693fd09bcd636086939688bb6091afe0907643
hanbi10 <jeonghanbi10@dgsw.hs.kr>
2025-11-03 19:32:45 +0900

    fix : 오류 수정

---
+++ src/main/java/com/example/oauth2/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.oauth2.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://localhost:3000") // 리액트 앱이 실행되는 출처 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
